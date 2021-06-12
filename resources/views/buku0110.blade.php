13eea37783c28f767892f727592f93262d79ed89
Faikaru <faikarbejo@gmail.com>
2021-06-12 12:14:15 +0700

    updated commit

--- resources/views/buku0110.blade.php
+++ resources/views/buku0110.blade.php
@@ -4,7 +4,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <title>
-    1461900206_KEGIATAN4_P
+    1461900110_EXPORT_DATA
   </title>
   <link rel="stylesheet" href="{{asset('css/app.css')}}" />
 </head>
